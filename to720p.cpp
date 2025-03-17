2aaed140c038a261251114f2933d71952398e82d
lxndrptkv <102393020+lxndrptkv@users.noreply.github.com>
2025-03-17 17:42:26 +0200

    update

--- to720p.cpp
+++ to720p.cpp
@@ -8,30 +8,32 @@ int main(int argc, char* argv[]) {
     }
     string inputFilePath = argv[1];
 
-
-    // slaga 720p do originalnoti ime na videoto
+    // Append "_720p24fpsCompressed" to the original video name
     string outputFilePath = inputFilePath;
     size_t dotPos = outputFilePath.find_last_of('.');
     if (dotPos != string::npos) {
-        outputFilePath.insert(dotPos, "_720p");
+        outputFilePath.insert(dotPos, "_720p24fpsCompressed");
     }
     else {
-        outputFilePath += "_720p";
+        outputFilePath += "_720p24fpsCompressed";
     }
-    //ffmpeg build line
-    string command = "ffmpeg -i \"" + inputFilePath + "\" -vf scale=-1:720 -c:v libx264 -preset fast -crf 23 -c:a copy \"" + outputFilePath + "\"";
 
-    //executva
-    cout << "Converting \"" << inputFilePath << "\" to 720p...\n";
+    // FFmpeg command with more compression
+    string command = "ffmpeg -i \"" + inputFilePath + "\" "
+        "-vf scale=-1:720 "          // 720p
+        "-r 24 "                     // 24 fps
+        "-c:v libx264 "              // H264 codec
+        "-preset slow "            // Slow preset for compression
+        "-crf 32 "                   // Higher CRF for more compression
+        "-c:a aac -b:a 64k "         // audio 
+        "\"" + outputFilePath + "\"";
+
+    // Execute
+    cout << "Converting \"" << inputFilePath << "\" to 720p with higher compression...\n";
     int result = system(command.c_str());
 
-    if (result == 0) {
-        cout << "Conversion successful! Output file: \"" << outputFilePath << "\"\n";
-    }
-    else {
-        cerr << "Error: Conversion failed.\n";
-    }
 
     return result;
+
 }
 
